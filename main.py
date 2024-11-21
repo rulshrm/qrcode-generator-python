e0b88615d3e5f63ebec0d4fd80aea5659be27e59
rulshrm <raflysahrul3114@gmail.com>
2024-11-21 18:54:11 +0700

    Add QR code generation functionality

---
+++ main.py
@@ -0,0 +1,14 @@
+import qrcode
+import qrcode.constants
+
+qr = qrcode.QRCode(
+  version=1,
+  error_correction=qrcode.constants.ERROR_CORRECT_L,
+  box_size=10,
+  border=4,
+)
+qr.add_data("text")
+qr.make(fit=True)
+
+img=qr.make_image(fill_color="black", back_color="white")
+img.save('Qrcode1.png')
