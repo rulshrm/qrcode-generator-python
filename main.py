340a3d83f56ee8db486a014c51a7175b126c8ea6
rulshrm <raflysahrul3114@gmail.com>
2024-11-21 23:14:41 +0700

    Update QR code input method to accept user input

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ qr = qrcode.QRCode(
   box_size=10,
   border=4,
 )
-qr.add_data("text")
+data = input('Masukan input: ')
+qr.add_data(data)
 qr.make(fit=True)
 
 img=qr.make_image(fill_color="black", back_color="white")
